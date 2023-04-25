d9966e04b29dc4877dbb0ea8b22256d9b9fae70d
nyasusho <cat.route20@gmail.com>
2023-04-25 23:07:52 +0900

    Add: foot link

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Exhaust';
+export const SITE_TITLE = 'nya2828';
 export const SITE_DESCRIPTION = 'サイトディスクリプション';
