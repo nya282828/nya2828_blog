127e28901b08132311b5d7d9fca3f302355b03df
nyasusho <cat.route20@gmail.com>
2023-05-20 08:42:28 +0900

    Add: theme toggle button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com/',
-	integrations: [
-		mdx({
-			extendMarkdownConfig: false,
-      smartypants: true,
-      gfm: true,
-		}), 
-		sitemap()
-	],
+  site: 'https://example.com/',
+  integrations: [mdx({
+    extendMarkdownConfig: false,
+    smartypants: true,
+    gfm: true
+  }), sitemap(), preact()]
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,7 @@
-import rss, {
-	pagesGlobToRssItems
-} from '@astrojs/rss';
+// import rss, {
+// 	pagesGlobToRssItems
+// } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
@@ -15,3 +16,13 @@ export async function get(context) {
     ),
 	});
 }
+
+// export async function get() {
+//   return rss({
+//     title: SITE_TITLE,
+//     description: SITE_DESCRIPTION,
+//     site: context.site,
+//     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+//     customData: `<language>en-us</language>`,
+//   });
+// }
