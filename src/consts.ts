fbf022efd00c99e4147be0e1e52dd33e20a7d426
nyasusho <cat.route20@gmail.com>
2023-06-27 22:45:18 +0900

    Update: head

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'nya2828';
-export const SITE_DESCRIPTION = 'サイトディスクリプション';
+export const SITE_DESCRIPTION = 'nya{susho}の個人ブログ。デザイン、Web開発、その他趣味など';
+export const TWITTER_SITE ='@nya2828';
