f6b32d874c834e3ef91af831ef38fc908d789a4d
nyasusho <cat.route20@gmail.com>
2023-08-15 01:06:39 +0900

    Update: font size style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import preact from "@astrojs/preact";
+import image from '@astrojs/image';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nya2828.com/',
   integrations: [mdx({
+    syntaxHighlight: 'shiki',
     extendMarkdownConfig: false,
     smartypants: true,
     gfm: true
-  }), sitemap(), preact()]
+  }), sitemap(), preact(), image()]
 });
