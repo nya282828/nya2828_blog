3f2f86929a853bba1ed516538e0ee21fc0cc35e6
nyasusho <cat.route20@gmail.com>
2024-04-01 03:24:29 +0900

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,4 +1,4 @@
-import rss from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
@@ -17,7 +17,8 @@ export async function GET(context) {
       description: post.data.description,
       customData: post.data.customData,
       link: `/blog/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body)),
+      // コンテンツ全文を出したい
+      // content: sanitizeHtml(parser.render(post.body)),
       // enclosure: {
       //   url: post.data.heroImage,
       //   length: 0
